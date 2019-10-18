66fb92e64f50a5c8a029393c0b1ce52bd8715c2c
fabianzielinski <fabz@o2.pl>
2019-10-18 03:51:46 +0200

    Add index.js function

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
 const formatDate = (timeInSeconds) => {
-  return `${timeInSeconds}s`;
-}
+  let formatedTime = `${timeInSeconds % 60}s`;
+
+  if (!timeInSeconds) {
+    return `0s`;
+  }
+
+  if (timeInSeconds % 3600 == 0) {
+    return `${Math.floor(timeInSeconds / 3600)}h`;
+  }
+
+  if (timeInSeconds > 60 && Math.floor((timeInSeconds % 3600) / 60) != 0) {
+    formatedTime =
+      `${Math.floor((timeInSeconds % 3600) / 60)}m ` + formatedTime;
+  }
+
+  if (timeInSeconds % 60 == 0) {
+    formatedTime = `${Math.floor((timeInSeconds % 3600) / 60)}m`;
+  }
+
+  if (timeInSeconds > 3600) {
+    return `${Math.floor(timeInSeconds / 3600)}h ` + formatedTime;
+  }
+
+  return formatedTime;
+};
 
 module.exports = formatDate;
